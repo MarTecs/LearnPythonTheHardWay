e916e63880fe092703e06bcdb43bf389dd87a71b
sivanWu0222 <sivan0222@vip.qq.com>
2018-01-29 17:32:19 +0800

    add

--- ex02.py
+++ ex02.py
@@ -1,4 +1,12 @@
 # -*-coding:utf-8-*-
 # Author: sivan
 # computer: notebook
-# description: 
+# description:
+
+# A comment,this is so you can read your program later.
+# Anything after the # is ignored by python
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code.
+# print("This won't run"）
+print("This will run.")
