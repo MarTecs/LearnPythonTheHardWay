eca28d8e6a43b39696eec5bfaa2871efff385e32
sivanWu0222 <sivan0222@vip.qq.com>
2018-01-29 20:36:12 +0800

    modify ex35.py

--- ex35.py
+++ ex35.py
@@ -7,18 +7,16 @@ from sys import exit
 
 def gold_room():
     print("This room is full of gold.How much do you take?")
-
-next = input(">")
-if "()" in next or "l" in next:
-    how_much = int(next)
-else:
-    dead("Man, learn to type a number.")
-
-if how_much < 50:
-    print("Nice, you're not greedy, you win!")
-    exit(0)
-else:
-    dead("You greedy bastard!")
+    next = input(">")
+    if "()" in next or "l" in next:
+        how_much = int(next)
+    else:
+        dead("Man, learn to type a number.")
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
 
 def bear_room():
     print("There is a bear here")
@@ -26,34 +24,31 @@ def bear_room():
     print("The fat bear is in front of another door.")
     print("How are you going to move the bear?")
     bear_moved = False
-
-while True:
-    next = input(">")
-
-if next == "take honey":
-    dead("The bear looks at you then slaps your face off.")
-elif next == "taunt bear" and not bear_moved:
-    print("The bear has moved from the door.You can go through it now.")
-    bear_moved = True
-elif next == "taunt bear" and bear_moved:
-    dead("The bear gets pissed off and chews your leg off.")
-elif next == "open door" and bear_moved:
-    gold_room()
-else:
-    print("I got no idea what that means.")
+    while True:
+        next = input(">")
+        if next == "take honey":
+            dead("The bear looks at you then slaps your face off.")
+        elif next == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door.You can go through it now.")
+            bear_moved = True
+        elif next == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif next == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("I got no idea what that means.")
 
 def cthulhu_room():
     print("Here you see the great evil Cthulhu.")
     print("He,it.whatever stares at you and yo go insane.")
     print("Do you flee for your life or eat your head?")
-
-next = input(">")
-if "flee" in next:
-    start()
-elif "head" in next:
-    dead("Wekk that was tasty!")
-else:
-    cthulhu_room()
+    next = input(">")
+    if "flee" in next:
+        start()
+    elif "head" in next:
+        dead("Wekk that was tasty!")
+    else:
+        cthulhu_room()
 
 def dead(why):
     print(why, "Good job!")
@@ -69,6 +64,5 @@ def start():
         cthulhu_room()
     else:
         dead("You stumble around the room until you starve.")
-
 
 start()
