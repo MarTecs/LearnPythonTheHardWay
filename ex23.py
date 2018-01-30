0815b92730ffcabaecc5507385637e912ce9f7fb
sivanWu0222 <sivan0222@vip.qq.com>
2018-01-30 13:31:28 +0800

    更多练习

---
+++ ex23.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 读代码
+
+## 1.通过浏览各个大型网站找到我们需要的Python代码
+    ### bitbucket.org
+    ### github.com
+    ### launchpad.net
+    ### koders.com
+## 2.通读代码，找到文件
+## 3.尝试理解你找到的代码
+
