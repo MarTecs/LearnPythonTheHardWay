5bcc1afb189d423fda48233fe4d264cb29fddd1f
sivanWu0222 <sivan0222@vip.qq.com>
2018-01-30 10:49:35 +0800

    modify python file

--- ex18.py
+++ ex18.py
@@ -4,7 +4,6 @@
 # description: 命名，变量，代码，函数
 
 # this one is like your scripts with argv
-
 def print_two(*args):
     arg1, arg2 = args
     print("arg1:%r，arg2:%r" % (arg1, arg2))
@@ -15,12 +14,12 @@ def print_two_again(arg1, arg2):
 
 # this just takes one argument
 def print_one(arg1):
-    print("arg1:%r" % arg1)
+    print("arg1:%s" % arg1)
 
 # this one takes no arguments
+## 没有任何参数的函数
 def print_none():
     print("I got nothing'.")
-
 
 
 print_two("Zed", "Shaw")
