01e4dcb9e308b148672e5dc7081e7d3a405bac76
sivanWu0222 <sivan0222@vip.qq.com>
2018-01-30 13:18:05 +0800

    add ex21.py

---
+++ ex21.py
@@ -0,0 +1,36 @@
+# -*-coding:utf-8-*-
+# Author: sivan
+# computer: notebook
+# description: 函数可以返回东西
+
+
+def add(a,b):
+    print("ADDING %d + %d" % (a,b))
+    return a + b
+
+def subtract(a,b):
+    print("SUBTRACTING %d - %d" % (a,b))
+    return a - b
+
+def multiply(a,b):
+    print("MULTIPLYING %d * %d" % (a,b))
+    return a * b
+
+def divide(a,b):
+    print("DIVIDING %d / %d" % (a,b))
+    return a / b
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print("Age：%d，Height：%d，Weight：%d，IQ：%d" % (age, height, weight, iq))
+
+
+# A puzzle for the extra credit, type it in anyway
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
